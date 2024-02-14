315b760dd4b098876f9459fcc4a6c03e1c510efe
Danrlei Gonçalves Zaffari <zaffaridam@gmail.com>
2024-02-14 15:21:41 -0300

    New commit

--- main.tf
+++ main.tf
@@ -17,9 +17,15 @@ resource "aws_instance" "app_server" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = "iac-alura"
-
+  user_data     = <<-EOF
+#!/bin/bash
+cd /home/ubuntu
+rm -rf /home/ubuntu/index.html
+echo "<h1>Feito com Terraform</h1>" > /home/ubuntu/index.html
+nohup busybox httpd -f -p 8080
+EOF
   tags = {
-    Name = "TerraformIaC"
+    Name = "TerraformIaCNew"
     Client = "Teste"
   }
 }
